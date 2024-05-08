c906a7e564f4e9e365c15288bb68cc6f328758b2
dasha gromova <darya.gromova8559@gmail.com>
2024-05-08 10:42:11 +0300

    init

--- src/dbmanager.py
+++ src/dbmanager.py
@@ -1,3 +1,17 @@
+import psycopg2
+
+from src.config import config
+
+
 class DBManager:
     def __init__(self):
         self.db_name = 'hh_company'
+
+    def execute_(self, query):
+        conn = psycopg2.connect(dbname=self.db_name, **config())
+        with conn:
+            with conn.cursor() as cur:
+                cur.execute(query)
+                results = cur.fetchall()
+        conn.close()
+        return results
