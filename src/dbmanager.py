c2f523677c9e38c91a132fd17966489049ccabf3
dasha gromova <darya.gromova8559@gmail.com>
2024-05-08 15:23:11 +0300

    init

--- src/dbmanager.py
+++ src/dbmanager.py
@@ -40,3 +40,19 @@ class DBManager:
         result = self.execute_(f'SELECT AVG(salary_from) AS "Средняя зарплата ОТ", '
                                f'AVG(salary_to) AS "Средняя зарплата ДО" FROM vacancies')
         return result
+
+
+    def get_vacancies_with_higher_salary(self):
+        """Получает список всех вакансий, у которых зарплата выше средней по всем вакансиям."""
+        result = self.execute_(f'SELECT * FROM vacancies '
+                               f'WHERE salary_from >(SELECT AVG(salary_from) '
+                               f'FROM vacancies) AND salary_to > (SELECT AVG(salary_to) FROM vacancies) '
+                               f'ORDER BY vacancy_id')
+        return result
+
+    def get_vacancies_with_keyword(self):
+        """Получает список всех вакансий, в названии которых содержатся переданные в метод слова, например python."""
+        result = self.execute_(f"SELECT * "
+                               f"FROM vacancies "
+                               f"WHERE vacancy_name LIKE '%инженер%'")
+        return result
